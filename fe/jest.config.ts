b6e08ec4a051b35845202a0f65caee8bd2fc3327
Jakub Murin <me@jakubmurin.dev>
2026-03-30 09:46:15 +0200

    style: format code for consistency in jest.config.ts, FileUpload.tsx, and tsconfig.test.json

--- fe/jest.config.ts
+++ fe/jest.config.ts
@@ -1,14 +1,14 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "./tsconfig.test.json" }],
-  },
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+	testEnvironment: "jsdom",
+	transform: {
+		"^.+\\.tsx?$": ["ts-jest", { tsconfig: "./tsconfig.test.json" }],
+	},
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/src/$1",
+	},
+	setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
 export default config;
